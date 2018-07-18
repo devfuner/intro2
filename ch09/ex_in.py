2679db7e43969221d113e84939bcf3e4a0af348a
devfuner <devfuner@google.com>
2018-07-18 14:19:33 +0900

    in 연산자   - 다른 자료형에 대한 예 추가

--- ch09/ex_in.py
+++ ch09/ex_in.py
@@ -1,13 +1,27 @@
 
 # in
-
 numbers = [1, 2, 3]
 print(1 in numbers)
-print(2 in numbers)
-print(3 in numbers)
+
+for n in numbers:
+    print(n)
 
 
 # not in
 print(4 in numbers)
 print(4 not in numbers)
 
+
+# tuple
+t = (1, 2, 3)
+print(1 in t)
+
+
+# set
+s = {1, 2, 3}
+print(1 in s)
+
+
+# dictionary
+d = {1: '1', 2: '2', 3: '3'}
+print(1 in d.keys())
