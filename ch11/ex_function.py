15b642dfc72073fab89179768d03b3f7244ca533
devfuner <devfuner@google.com>
2018-07-21 09:42:03 +0900

    주석 위치 변경

--- ch11/ex_function.py
+++ ch11/ex_function.py
@@ -18,8 +18,10 @@ def return_none():  # 반환값이 없는 함수
 # def는 뭐지?
 # 함수를 만들겠다는 예약어(키워드)
 
-sum_value = plus(9, 5)  # sum_value는 sum(9, 5)의 결과값을 대입 받는다.
-print(sum_value)  # print(sum_value)는 sum_value의 값을 화면에 출력한다.
+# sum_value는 sum(9, 5)의 결과값을 대입 받는다.
+sum_value = plus(9, 5)
+# print(sum_value)는 sum_value의 값을 화면에 출력한다.
+print(sum_value)
 
 # 결과값은 함수에서 리턴되는 값
 # 또는 어떤 식의 계산된 결과값
